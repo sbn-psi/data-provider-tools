f00e77a38330ebc3e50b2ee99a58fc3835f7d413
Jesse Stone <jstone@psi.edu>
2018-03-28 10:32:38 -0700

    Extracted submethods for easier reading

--- image_gallery/image_gallery.py
+++ image_gallery/image_gallery.py
@@ -1,10 +1,7 @@
 import sys
 import os
 
-def main():
-    path = sys.argv[1]
-    images = [f for f in (os.listdir(path)) if f.endswith(".png")]
-
+def print_header():
     print "<head></head>"
 
     print "<body>"
@@ -13,16 +10,34 @@ def main():
     print "<h2>Introduction</h2>"
     print "<p>Intro text goes here</p>"
 
+
+def print_toc(images):
     print "<h2>Table of Images</h2>"
     print "<a id='toc'/>"
     for i in images:
         base = i.replace('.png', '')
         print "<div><a href='#%s'>%s</a></div>" % (i,base)
 
+def print_gallery(images):
     for i in images:
         base = i.replace('.png', '')
         print "<div style='page-break-before:always'><a id='%s'/><h2>%s</h2><img src='%s'/></div>" % (i,base,i)
+
+
+def print_footer():
     print "</body>"
+
+
+def main():
+    path = sys.argv[1]
+    images = [f for f in (os.listdir(path)) if f.endswith(".png")]
+
+    print_header()
+    print_toc(images)
+    print_gallery(images)
+    print_footer()
+
+
 
 if __name__ == "__main__":
     main()
