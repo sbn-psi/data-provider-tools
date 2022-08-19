2ddb39f30d20831fdf5934355ded0e57f4b78cad
Jesse Stone <jstone@psi.edu>
2022-08-19 14:14:49 -0700

    A script to populate xml labels from CSV files

---
+++ template_filler/template_filler.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import csv
+import re
+import sys
+import argparse
+import jinja2
+import os.path
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--template-file", help="The path to the template file used to generate the labels", required=True)
+    parser.add_argument("--csv-file", help="The path to the CSV file that will supply values for the templates", required=True)
+    parser.add_argument("--output-path", help="The directory where the generated labels will go.", required=True)
+    args = parser.parse_args()
+
+    template_filename = os.path.basename(args.template_file)
+    specified_template_directory = os.path.dirname(args.template_file)
+    template_directory = specified_template_directory if os.path.isabs(specified_template_directory) else os.path.join(".", specified_template_directory)
+    template_loader = jinja2.FileSystemLoader(template_directory)
+    environment = jinja2.Environment(loader=template_loader, autoescape=jinja2.select_autoescape())
+    template = environment.get_template(template_filename)
+
+    with open(args.csv_file) as f:
+        reader = csv.DictReader(f)
+        for d in reader:
+            filename = d["filename"]
+            contents = template.render(d)
+            with open(os.path.join(args.output_path, filename), "w") as outfile:
+                outfile.write(contents)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
