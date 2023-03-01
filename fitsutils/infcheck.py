5df8ebb4ea794643440a8f47655da0fc50129b9a
Jesse Stone <jstone@psi.edu>
2023-03-01 13:36:34 -0700

    Added a check for non-image files

--- fitsutils/infcheck.py
+++ fitsutils/infcheck.py
@@ -12,19 +12,23 @@ def main():
     for filename in args.filenames:
         hdus = fits.open(filename)
         data = hdus[0].data
-        flat = data.flatten()
-        print ("-" * 10, filename, "-" * 10)
-        print("    min/max", flat.min(), flat.max())
-        print("    min/max without nans", flat[numpy.nanargmin(flat)], flat[numpy.nanargmax(flat)])
-        
-        posinfs = numpy.argwhere(numpy.isposinf(data))
-        print("    +infs", len(posinfs), posinfs)
-        
-        neginfs = numpy.argwhere(numpy.isneginf(data))
-        print("    -infs", len(neginfs), neginfs)
 
-        nans = numpy.argwhere(numpy.isnan(data))
-        print("    nans", len(nans), nans)
+        if data is None:
+            print (f"{filename}: not an image")
+        else:
+            flat = data.flatten()
+            print ("-" * 10, filename, "-" * 10)
+            print("    min/max", flat.min(), flat.max())
+            print("    min/max without nans", flat[numpy.nanargmin(flat)], flat[numpy.nanargmax(flat)])
+            
+            posinfs = numpy.argwhere(numpy.isposinf(data))
+            print("    +infs", len(posinfs), posinfs)
+            
+            neginfs = numpy.argwhere(numpy.isneginf(data))
+            print("    -infs", len(neginfs), neginfs)
+
+            nans = numpy.argwhere(numpy.isnan(data))
+            print("    nans", len(nans), nans)
 
 if __name__ == '__main__':
     main()
