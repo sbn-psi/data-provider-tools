f31cd44fb1d50b27b753996ba88aa1290f5e6176
Jesse Stone <jstone@psi.edu>
2023-03-01 14:44:25 -0700

    made the results display more consistent between types

--- fitsutils/infcheck.py
+++ fitsutils/infcheck.py
@@ -31,35 +31,30 @@ def process_image(hdu, filename, index):
     if data is None:
         print (f"{filename}.{index}: Not an image")
     else:
-        posinfs = numpy.argwhere(numpy.isposinf(data))
-        neginfs = numpy.argwhere(numpy.isneginf(data))
-        nans = numpy.argwhere(numpy.isnan(data))
+        posinfs = len(numpy.argwhere(numpy.isposinf(data)))
+        neginfs = len(numpy.argwhere(numpy.isneginf(data)))
+        nans = len(numpy.argwhere(numpy.isnan(data)))
+        show_result(filename, index, posinfs, neginfs, nans)
 
-        if len(posinfs) == 0 and len(neginfs) == 0 and len(nans) == 0:
-            print (f"{filename}.{index}: OK")
-        else:
-            if len(posinfs) > 0:
-                print (f"{filename}.{index}: {len(posinfs)} positive infinities detected: {posinfs}")
-            if len(neginfs) > 0:
-                print (f"{filename}.{index}: {len(neginfs)} negative infinities detected: {neginfs}")
-            if len(nans) > 0:
-                print (f"{filename}.{index}: {len(nans)} not-a-numbers detected: {nans}")
+def show_result(filename, index, posinfs, neginfs, nans):
+    if posinfs == 0 and neginfs == 0 and nans == 0:
+        print (f"{filename}.{index}: OK")
+    else:
+        if posinfs > 0:
+            print (f"{filename}.{index}: {len(posinfs)} positive infinities detected")
+        if neginfs > 0:
+            print (f"{filename}.{index}: {len(neginfs)} negative infinities detected")
+        if len(nans) > 0:
+            print (f"{filename}.{index}: {len(nans)} not-a-numbers detected")
 
+    
 def process_table(hdu, filename, index):
     data = hdu.data
     if data is None:
         print (f"{filename}.{index}: Not a table")
     else:
         posinfs, neginfs, nans = handle_data(data)
-        if posinfs == 0 and neginfs == 0 and nans == 0:
-            print (f"{filename}.{index}: OK")
-        else:
-            if posinfs > 0:
-                print (f"{filename}.{index}: {len(posinfs)} positive infinities detected")
-            if neginfs > 0:
-                print (f"{filename}.{index}: {len(neginfs)} negative infinities detected")
-            if len(nans) > 0:
-                print (f"{filename}.{index}: {len(nans)} not-a-numbers detected")
+        show_result(filename, index, posinfs, neginfs, nans)
 
 def handle_data(data):
     stats = [handle_row(row) for row in data]
