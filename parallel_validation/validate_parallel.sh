08dcd3914ac846f740714b8c3a1b4951a42e8a87
Jesse Stone <jstone@psi.edu>
2024-10-02 09:33:51 -0700

    added usage string

--- parallel_validation/validate_parallel.sh
+++ parallel_validation/validate_parallel.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 set -e
 
+
 DIRNAME=$1
 CONFIG_FILE=$2
 CATALOG_FILE=$3
@@ -9,26 +10,34 @@ WORKERS=${5-4}
 BATCH_SIZE=${6-1000}
 
 
+usage()
+{
+    echo "usage: validate_parallel.sh validation_directory config_file catalog_file report_dir [workers] [batch_size]"
+    exit 1
+}
+
+
 if [ -z "$DIRNAME" ]; then
     echo 'no validation direcotry specified'
-    exit
+    usage
 fi
 
 
 if [ -z "$CONFIG_FILE" ]; then
     echo 'no config file specified'
-    exit
+    usage
 fi
+
 
 if [ -z "$CATALOG_FILE" ]; then
     echo 'no catalog file specified'
-    exit
+    usage
 fi
 
 
 if [ -z "$DIRNAME" ]; then
     echo 'no report directory specified'
-    exit
+    usage
 fi
 
 
@@ -38,3 +47,5 @@ mkdir -p "$REPORT_DIR/complete"
 [ -d "$DIRNAME" ] || (echo "Directory name not a directory" && exit 1)
 
 find "$DIRNAME" -name "*.xml" -print0 | xargs -0 -n"${BATCH_SIZE}" -P"${WORKERS}" ./validate_worker.sh "$CONFIG_FILE" "$CATALOG_FILE" "$REPORT_DIR"
+
+
