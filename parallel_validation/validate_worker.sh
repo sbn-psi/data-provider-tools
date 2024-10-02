ce723e427a7d25ccd14aa661563514b320136c7f
Jesse Stone <jstone@psi.edu>
2024-10-02 09:53:30 -0700

    fixed validation, made parameter parsing more compact

--- parallel_validation/validate_worker.sh
+++ parallel_validation/validate_worker.sh
@@ -6,21 +6,19 @@ usage()
     exit 1
 }
 
+CONFIG_FILE="$1"; shift
+CATALOG_FILE="$1"; shift
+REPORT_DIR="$1"; shift
 
-CONFIG_FILE=$1
-shift
-CATALOG_FILE=$1
-shift
-REPORT_DIR=$1
-shift
+set -e
 
-[ -n "$CONFIG_FILE" ] || (echo "Config file not provided" && usage)
-[ -n "$CATALOG_FILE" ] || (echo "Catalog file not provided" && usage)
-[ -n "$REPORT_DIR" ] || (echo "Report directory not provided" && usage)
-[ -d "$REPORT_DIR" ] || (echo "Report directory does not exist" && usage)
+[ -n "$CONFIG_FILE" ] || (echo "Config file not provided"; usage)
+[ -n "$CATALOG_FILE" ] || (echo "Catalog file not provided"; usage)
+[ -n "$REPORT_DIR" ] || (echo "Report directory not provided"; usage)
+[ -d "$REPORT_DIR" ] || (echo "Report directory does not exist"; usage)
 
 RUNDATE=$(date +'%Y%m%dT%H%M%S')
-REPORT_FILE=$(mktemp "$REPORT_DIR/validation.$RUNDATE.XXXXXX.txt")
+REPORT_FILE=$(mktemp "$REPORT_DIR/validation.$RUNDATE".XXXXXX.txt)
 
 validate -c "$CONFIG_FILE" -C "$CATALOG_FILE" -t "$@" > "$REPORT_FILE"
 mv "$REPORT_FILE" "$REPORT_DIR/complete"
