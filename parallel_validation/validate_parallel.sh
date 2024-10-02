e194e025e8af20a883b8850cdfbb0c1c8263b16d
Jesse Stone <jstone@psi.edu>
2024-10-02 09:36:39 -0700

    made parameter validation more compact

--- parallel_validation/validate_parallel.sh
+++ parallel_validation/validate_parallel.sh
@@ -2,6 +2,13 @@
 set -e
 
 
+usage()
+{
+    echo "usage: validate_parallel.sh validation_directory config_file catalog_file report_dir [workers] [batch_size]"
+    exit 1
+}
+
+
 DIRNAME=$1
 CONFIG_FILE=$2
 CATALOG_FILE=$3
@@ -10,41 +17,13 @@ WORKERS=${5-4}
 BATCH_SIZE=${6-1000}
 
 
-usage()
-{
-    echo "usage: validate_parallel.sh validation_directory config_file catalog_file report_dir [workers] [batch_size]"
-    exit 1
-}
-
-
-if [ -z "$DIRNAME" ]; then
-    echo 'no validation direcotry specified'
-    usage
-fi
-
-
-if [ -z "$CONFIG_FILE" ]; then
-    echo 'no config file specified'
-    usage
-fi
-
-
-if [ -z "$CATALOG_FILE" ]; then
-    echo 'no catalog file specified'
-    usage
-fi
-
-
-if [ -z "$DIRNAME" ]; then
-    echo 'no report directory specified'
-    usage
-fi
-
+[ -n "$DIRNAME" ] || (echo "Directory name not provided" && usage)
+[ -d "$DIRNAME" ] || (echo "Directory name not a directory" && usage)
+[ -n "$CONFIG_FILE" ] || (echo "Config file not provided" && usage)
+[ -n "$CATALOG_FILE" ] || (echo "Catalog file not provided" && usage)
+[ -n "$REPORT_DIR" ] || (echo "Report directory not provided" && usage)
 
 mkdir -p "$REPORT_DIR/complete"
-
-[ -n "$DIRNAME" ] || (echo "Directory name not provided" && exit 1)
-[ -d "$DIRNAME" ] || (echo "Directory name not a directory" && exit 1)
 
 find "$DIRNAME" -name "*.xml" -print0 | xargs -0 -n"${BATCH_SIZE}" -P"${WORKERS}" ./validate_worker.sh "$CONFIG_FILE" "$CATALOG_FILE" "$REPORT_DIR"
 
